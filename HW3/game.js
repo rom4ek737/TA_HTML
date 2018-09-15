607ecc1da3590c232ede89d550c321b369df1ae1
rom4ek737 <rom4ek737@users.noreply.github.com>
2018-09-15 23:06:29 +0300

    add 3 hw

---
+++ HW3/game.js
@@ -0,0 +1,31 @@
+﻿var count = 0;
+var userNumber;
+var randomNumber = Math.floor(Math.random() * Math.floor(5) + 1);
+var playTheGame = confirm("Play the game?");
+var bool = false;
+
+//console.log(randomNumber);
+if (playTheGame) {
+  while(!bool) {
+    userNumber = prompt("Please, enter the number from 1 to 5:");
+    if (userNumber < 1 || userNumber > 5) {
+      alert("Please, enter the valid number");
+    }
+    else {
+      count ++;
+     if (count < 3) {
+      if (userNumber == randomNumber) {
+        alert("Congratulations!You are winner!");
+        bool = true;
+     }
+      }
+      else {
+        alert("Today is not your day.");
+        bool = true;
+      }
+    }
+  }
+}
+else {
+  alert("Not today");
+}
---
+++ HW3/prime_numbers.js
@@ -0,0 +1,19 @@
+﻿var i;
+var j;
+var bool = true;
+  function resolve() {
+    for (i = 2; i <= 20; i ++) {
+      for (j = 2; j < i; j ++) {
+       if (i % j === 0) {
+         bool = false;
+         break;
+       }  
+      }
+      if (bool) {
+        console.log(i);
+      }
+      else bool = true;
+    }
+  }
+
+resolve();
